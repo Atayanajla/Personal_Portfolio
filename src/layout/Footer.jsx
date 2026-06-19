1669e7336ea53d27421d3846e55b0c3d237bfdd0
Atayanajla <atayanajla269@gmail.com>
2026-06-19 21:42:14 +0700

    footer2

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="w-full py-8 border-t border-slate-200 dark:border-slate-800/50 mt-10">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4 text-sm ">
-        <p>Designed & Developed by Ataya Najla.</p>
+        <p>Developed by Ataya Najla.</p>
         <p className="text-xs">
           Built with <span className="text-blue-500">React</span> &{" "}
           <span className="text-teal-500">Tailwind CSS</span>
